2d200bb214c8b1b606097f9cd9bdefabd6da90cb
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-18 00:08:28 +0530

    lovecalculator.js

--- LoveCalculator.js
+++ LoveCalculator.js
@@ -1,1 +1,18 @@
 // code snippet
+
+prompt("Your Name");
+
+var rand = Math.random() * 100;
+
+rand = Math.floor(rand) + 1;
+
+// console.log(rand)
+
+if(rand >= 75){
+    alert("Your Love score is " + rand + "%" + " Your love is like Romeo and Juliet");    
+}
+else{
+    alert("Your Love score is " + rand + "%");
+}
+
+
