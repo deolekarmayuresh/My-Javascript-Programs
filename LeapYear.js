1fd276e7378c7114d794b02a46d4d5819ce9240e
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-25 21:23:34 +0530

    leap year

---
+++ LeapYear.js
@@ -0,0 +1,20 @@
+// code snippet for calculating leap year
+
+function isLeap(year) {
+
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                return ("It's a Leap Year");
+            }else{
+                return ("Not a Leap Year");
+            }
+        }else{
+            return ("It's a Leap Year");
+        }
+    }else{
+        return ("Not a Leap Year");
+    }
+}
+
+isLeap(2002);
