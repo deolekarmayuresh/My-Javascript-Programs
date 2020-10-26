989b27f286fde7020f805310167eabbbfebfc153
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-26 13:58:41 +0530

    fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,2 +1,30 @@
 // code snippet for FizzBuzz
 
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+//     while(count <= 100) {
+        
+    for(count = 1; count < 101; count++){
+    if(count % 3 === 0 && count % 5 === 0){
+            output.push("FizzBuzz");
+        }
+    else if(count % 3 === 0){
+        output.push("Fizz");
+    }
+       else if(count % 5 === 0) {
+            output.push("Buzz");
+        }
+        
+    else {
+        output.push(count);
+    }
+
+    count++;
+    console.log(output);
+    }
+    }    
+// }
+
+// fizzBuzz();
